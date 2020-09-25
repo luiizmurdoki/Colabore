e7ee95d6c493ebf062f30dff47f9e06bec912264
Edu <luizeduardomiranda2009@gmail.com>
2020-09-24 23:42:39 -0300

    finished bundle and onpen description activity

--- app/src/main/java/com/example/colabore/ui/description/DescriptionActivity.kt
+++ app/src/main/java/com/example/colabore/ui/description/DescriptionActivity.kt
@@ -1,24 +1,23 @@
 package com.example.colabore.ui.description
 
+import android.content.Intent
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.colabore.R
-import com.example.colabore.model.PersistUserInformation.cpf
 import com.example.colabore.ui.base.BaseActivity
 import com.example.colabore.ui.dialogmessage.MessageBottomDialog
-import com.example.colabore.ui.main.MainContract
-import com.example.colabore.ui.main.MainPresenter
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_ngo_description.*
-import kotlinx.android.synthetic.main.item_card.view.*
+
 
 class DescriptionActivity :  BaseActivity(), DescriptionContract.View {
     private lateinit var auth: FirebaseAuth
-    private val url = "https://scontent.fcgh37-1.fna.fbcdn.net/v/t1.0-9/25498233_304732076683737_2571184881809077165_n.png?_nc_cat=109&_nc_sid=85a577&_nc_ohc=bT8dJjmbih8AX8wkFrv&_nc_ht=scontent.fcgh37-1.fna&oh=c5a1a6b488dcb53a61dfbb0a52be54fa&oe=5F7EE42F"
-    private val title = "Centro de Inclusão Digital"
-    private val desc = "Objetivo Promover a Inclusão Digital de Jovens no Mercado de Informática, conscientização da Preservação do Meio Ambiente e Cidadania, através da Reciclagem de Equipamentos de Informática.  O projeto esta dividido em três fases, cidadania, meio ambiente e técnicas de informática:  Fase 1 – Cidadania Nesta primeira fase o aluno obterá conhecimentos sobre cidadania, direitos e deveres do cidadão, Ética profissional, governabilidade, viver em comunidade, com duração de 02horas/aula.  Fase 2 – Meio ambiente Nesta fase o aluno aprendera os conceitos de Preservação do Meio Ambiente, Causas e Efeitos à Natureza, Legislação e Reciclagem de Equipamentos de Informática, com duração de 02 horas.  Fase 3 – Curso Técnico em Informática Nesta fase o aluno terá aulas praticas com acompanhamento apostilado de Noções básicas de Informática, Montagem e Manutenção de Microcomputadores."
+    private lateinit var url: String
+    private lateinit var title: String
+    private lateinit var desc: String
+
 
     private val presenter: DescriptionContract.Presenter by lazy {
         DescriptionPresenter().apply { attachView(this@DescriptionActivity) }
@@ -28,16 +27,27 @@ class DescriptionActivity :  BaseActivity(), DescriptionContract.View {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ngo_description)
         auth = FirebaseAuth.getInstance()
+        getExtras()
         displayDescription()
         FirebaseApp.initializeApp(this)
 
     }
 
+    private fun getExtras(){
+        url = intent.getStringExtra("url")
+        title = intent.getStringExtra("nome")
+        desc = intent.getStringExtra("info")
+    }
+
 
     private fun displayDescription(){
-        Glide.with(this).load(url).apply(RequestOptions().error(R.drawable.ic_default_empty).placeholder(R.drawable.ic_default_empty)).into(imageNgoIv)
-        ngoName.text = title
-        drescripitionTv.text = desc
+        Glide.with(this).load(url).apply(
+            RequestOptions().error(R.drawable.ic_default_empty).placeholder(
+                R.drawable.ic_default_empty
+            )
+        ).into(imageNgoIv)
+        ngoName.text = title.toString()
+        drescripitionTv.text = desc.toString()
     }
 
     override fun displayError(msg: String?){
--- app/src/main/java/com/example/colabore/ui/main/adapter/MainCardAdapter.kt
+++ app/src/main/java/com/example/colabore/ui/main/adapter/MainCardAdapter.kt
@@ -1,30 +1,18 @@
 package com.example.colabore.ui.main.adapter
 
-import android.R.attr
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory.decodeStream
-import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.net.toUri
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.colabore.R
 import com.example.colabore.model.CardModel
 import com.example.colabore.ui.base.SimpleBaseRecyclerViewAdapter
 import com.example.colabore.ui.description.DescriptionActivity
-import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.item_card.view.*
-import kotlinx.android.synthetic.main.placeholder_default_general.view.*
-import org.jetbrains.anko.image
-import org.jetbrains.anko.imageURI
-import org.jetbrains.anko.sdk25.coroutines.onClick
-import java.io.InputStream
-import java.net.HttpURLConnection
-import java.net.URL
 
 
 var context = Activity()
@@ -58,10 +46,16 @@ class MainCardAdapter(context: Context) : SimpleBaseRecyclerViewAdapter(context)
         return ItemViewHolder(itemView)
     }
 
-    inner class ItemViewHolder(itemView: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(itemView) {
+    inner class ItemViewHolder(itemView: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(
+        itemView
+    ) {
         fun bind(item: CardModel) {
             itemView.apply {
-                Glide.with(this).load(item.imageUrl).apply(RequestOptions().error(R.drawable.ic_default_empty).placeholder(R.drawable.ic_default_empty)).into(homeImageIv)
+                Glide.with(this).load(item.imageUrl).apply(
+                    RequestOptions().error(R.drawable.ic_default_empty).placeholder(
+                        R.drawable.ic_default_empty
+                    )
+                ).into(homeImageIv)
                 ongTextTv.text = item.nome
 
                 cardCv.setOnClickListener {
@@ -70,6 +64,9 @@ class MainCardAdapter(context: Context) : SimpleBaseRecyclerViewAdapter(context)
                     val url =item.imageUrl
 
                     val intent = Intent(context, DescriptionActivity::class.java)
+                    intent.putExtra("nome", title)
+                    intent.putExtra("info", desc)
+                    intent.putExtra("url",url)
                     context.startActivity(intent)
                 }
 
